3e326b5b2b8f5835876ab6aec15868924676fc23
jeffreywhite@gmail.com <jeffreywhite@gmail.com>
2023-12-09 11:55:47 +0100

    Added parameter validation and testing

--- ItemInventory.py
+++ ItemInventory.py
@@ -2,38 +2,46 @@ import unittest
 from SalesItem import SalesItem
 from SalesItemType import SalesItemType
 
+
 class ItemInventory(object):
     def __init__(self):
         self.items = []
 
-    def addItemToInventory(self, newitem):
-
+    def addItemToInventory(self, newitem: SalesItem):
+        assert isinstance(newitem, SalesItem), 'invalid SalesItem provided'
         if newitem.itemType in [items.itemType for items in self.items]:
             print("Item " + newitem.itemType.name + " already in Inventory")
         else:
             self.items.append(newitem)
             print("Item " + newitem.itemType.name + " added to Inventory")
 
-    def findItem(self, itemType):
+    def findItem(self, itemType: SalesItemType) -> SalesItem:
+        assert isinstance(itemType, SalesItemType), 'invalid SalesItemType provided'
         inventoryItem = [itemFound for itemFound in self.items if itemType == itemFound.itemType]
         if inventoryItem.__len__() < 1:
             return None
         else:
             return inventoryItem[0]
 
-    def itemCount(self):
+    def itemCount(self) -> int:
         return self.items.__len__()
 
 class TestItemInventory(unittest.TestCase):
 
     def testItemInventory(self):
         itemInventory = ItemInventory()
+        # Test for invalid parameter type (should be SalesItem not string)
+        with self.assertRaises(AssertionError):
+            itemInventory.addItemToInventory("apple")
         itemInventory.addItemToInventory(SalesItem(SalesItemType.APPLE, 45, 1))
         itemInventory.addItemToInventory(SalesItem(SalesItemType.ORANGE, 65, 1))
         self.assertEqual(itemInventory.items.__len__(), 2, "ItemInventory has incorrect number of items")
         itemInventory.addItemToInventory(SalesItem(SalesItemType.APPLE, 45, 1))
         self.assertEqual(itemInventory.items.__len__(), 2, "ItemInventory has incorrect number of items")
         print([itemFound for itemFound in itemInventory.items])
+        # Test for invalid parameter type (should be SalesItemType not string)
+        with self.assertRaises(AssertionError):
+            itemInventory.findItem("apple")
         orange = itemInventory.findItem(SalesItemType.ORANGE)
         print("Orange found ", orange.itemType)
         self.assertEqual(orange.itemType, SalesItemType.ORANGE, "ORANGE not found in Inventory")
--- ShoppingCart.py
+++ ShoppingCart.py
@@ -4,21 +4,25 @@ from SalesItem import SalesItem
 from SalesItemType import SalesItemType
 
 class ShoppingCart(object):
-    def __init__(self, itemInventory):
+    def __init__(self, itemInventory: ItemInventory):
+        assert isinstance(itemInventory, ItemInventory), 'invalid ItemInventory provided'
         self.items = []
         self.itemInventory = itemInventory
 
-    def findItem(self, itemType):
+    def findItem(self, itemType: SalesItemType) -> SalesItem:
+        assert isinstance(itemType, SalesItemType), 'invalid SalesItemType provided'
         salesItem = [itemFound for itemFound in self.items if itemType == itemFound.itemType]
         if salesItem.__len__() < 1:
             return None
         else:
             return salesItem[0]
 
-    def addSingleSalesItem(self, itemType):
+    def addSingleSalesItem(self, itemType: SalesItemType):
         self.addSalesItem(itemType, 1)
 
-    def addSalesItem(self, itemType, count):
+    def addSalesItem(self, itemType: SalesItemType, count: int):
+        assert isinstance(count, int), 'count must be integer value'
+        assert isinstance(itemType, SalesItemType), 'invalid SalesItemType provided'
         newitem = self.findItem(itemType)
         if newitem is None :
             newitem = self.itemInventory.findItem(itemType)
@@ -32,13 +36,13 @@ class ShoppingCart(object):
             print("Item " + itemType.name + " already in ShoppingCart, increment items by " + str(count))
             newitem.itemCount = newitem.itemCount + count
 
-    def itemCount(self):
+    def itemCount(self) -> int:
         return sum([item.itemCount for item in self.items])
 
-    def totalise(self):
+    def totalise(self) -> int:
         return sum([itemFound.itemCount * itemFound.priceIn100s for itemFound in self.items])
 
-    def totalFormattedInPounds(self):
+    def totalFormattedInPounds(self) -> str:
         pounds = self.totalise()/100
         pence = self.totalise()%100
         return format("%d.%2d" % (pounds, pence))
@@ -55,6 +59,12 @@ class TestCart(unittest.TestCase):
         self.assertEqual(_itemInventory.itemCount(), 2, "ItemInventory has incorrect number of items")
         _shoppingCart = ShoppingCart(_itemInventory)
         self.assertEqual(_shoppingCart.itemCount(), 0, "Cart has incorrect number of products")
+        # Test for invalid parameter type (should be SalesItemType - APPLE or ORANGE)
+        with self.assertRaises(AssertionError):
+            _shoppingCart.addSingleSalesItem(3)
+        # Test for invalid parameter type (should be integer count not string)
+        with self.assertRaises(AssertionError):
+            _shoppingCart.addSalesItem(SalesItemType.APPLE, "three")
         _shoppingCart.addSingleSalesItem(SalesItemType.APPLE)
         _shoppingCart.addSingleSalesItem(SalesItemType.APPLE)
         _shoppingCart.addSingleSalesItem(SalesItemType.APPLE)
